1e58874fb6ef3522a8171dbcac89fe291ca1bfa9
cbwnyx <mominepoomp@gmail.com>
2024-03-24 12:40:38 -0700

    added basic guard movement

--- Gibby's Escape/Assets/Scripts/GuardController.cs
+++ Gibby's Escape/Assets/Scripts/GuardController.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class GuardController : MonoBehaviour
 {
     public bool GuardMoved;
+    public Rigidbody rb;
+    public float speed = 5f;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
+    {
+        GuardMoved = false;
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
     {
         GuardMove();
     }
 
     private void GuardMove()
     {
-        if (GuardMoved = false)
+        if (GuardMoved == false)
         {
             StartCoroutine(GuardWalk());
         }
@@ -23,10 +29,14 @@ public class GuardController : MonoBehaviour
     public IEnumerator GuardWalk()
     {
         GuardMoved = true;
-        transform.Translate(Vector3.forward);
+        rb.velocity = speed * (Vector3.forward);
         yield return new WaitForSeconds(2f);
-        transform.Translate(Vector3.back);
+        transform.Rotate(new Vector3(0f, 180f, 0f));
+        yield return new WaitForSeconds(1f);
+        rb.velocity = speed * (Vector3.back);
         yield return new WaitForSeconds(2f);
+        transform.Rotate(new Vector3(0f, 180f, 0f));
+        yield return new WaitForSeconds(1f);
         GuardMoved = false;
     }
 }
